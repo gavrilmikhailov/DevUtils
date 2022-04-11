2fef82a11bf6cf1035d7ff017b8bb122a7b05e90
Gavril Mikhailov <gavrilmikhailov@gmail.com>
2022-04-11 22:59:27 +0900

    Added collectionView prototype

--- Packages/Themes/Sources/Themes/ViewController.swift
+++ Packages/Themes/Sources/Themes/ViewController.swift
@@ -9,12 +9,35 @@ import AppKit
 
 final class ViewController: NSViewController {
     
+    private lazy var collectionView: NSCollectionView = {
+        let flowLayout = NSCollectionViewFlowLayout()
+        flowLayout.itemSize = NSSize(width: 120, height: 120)
+        let collectionView = NSCollectionView()
+        collectionView.collectionViewLayout = flowLayout
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
     init() {
         super.init(nibName: nil, bundle: nil)
     }
     
     override func loadView() {
         view = NSView(frame: .zero)
+        view.wantsLayer = true
+        view.addSubview(collectionView)
+
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    fileprivate func extractedFunc() {
         do {
             let libraryURL = try FileManager.default.url(for: .libraryDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
             let themesURL = libraryURL
@@ -22,14 +45,14 @@ final class ViewController: NSViewController {
                 .appendingPathComponent("XCode")
                 .appendingPathComponent("UserData")
                 .appendingPathComponent("FontAndColorThemes")
-
+            
             if !FileManager.default.fileExists(atPath: themesURL.path) {
                 try FileManager.default.createDirectory(at: themesURL, withIntermediateDirectories: false)
             }
             let contents = try FileManager.default
                 .contentsOfDirectory(at: themesURL, includingPropertiesForKeys: nil)
                 .filter { $0.pathExtension == "xccolortheme" }
-
+            
             for file in contents {
                 print(file.path)
             }
@@ -40,5 +63,31 @@ final class ViewController: NSViewController {
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
+
+extension ViewController: NSCollectionViewDataSource {
+    func collectionView(_ collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    func collectionView(_ collectionView: NSCollectionView, itemForRepresentedObjectAt indexPath: IndexPath) -> NSCollectionViewItem {
+        let item = CollectionViewItem(nibName: nil, bundle: nil)
+        item.view.wantsLayer = true
+        item.view.layer?.backgroundColor = NSColor.green.cgColor
+        return item
+    }
+}
+
+extension ViewController: NSCollectionViewDelegate {
+    
+}
+
+final class CollectionViewItem: NSCollectionViewItem {
+    
+    override func loadView() {
+        view = NSView(frame: .zero)
     }
 }
